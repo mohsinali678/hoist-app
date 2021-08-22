f1600386c93e4651c4f9b83ad3c2cf7e02d22c36
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-08-22 17:52:23 -0400

    completed questionare

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState ,useEffect} from 'react'
-
+import {useHistory} from "react-router-dom";
 function Questionare () {
     const [questions, setQuestions] = useState({
         weight: false,
@@ -12,49 +12,55 @@ function Questionare () {
         bloodOxygen: false,
         sleepAmount: false
     });
-
+    let history = useHistory();
     const handleSubmit = (e) => {
-        if (e.target.value === 'yes') {
-            setQuestion1({
-                q1: "How many cardio did you burn today?",
-                q2: "",
-                q3: "",
-                q4: "",
-                q5: ""
-            }) 
+        const selectedQuestions = []
+        for (let question in questions) {
+            if (question) {
+                selectedQuestions.push(question)
+            }
         }
 
+        history.push("/new-user-form", {questions: selectedQuestions})
       }
 
+    const handleCheckbox = (e) => {
+        setQuestions((prevState) => {
+            return {
+                ...prevState, 
+                [e.target.name]: !e.target.value
 
+            }
+        })
+    }
 
     
     return (
     <section>
         <h2>What do you want to focus on?</h2>
         <form onSubmit={handleSubmit}> 
-            <input type="checkbox" id="weight" name="weight" value="true"/>
+            <input type="checkbox" id="weight" name="weight" onClick={handleCheckbox} value={questions.weight}/>
             <label for="weight">Weight</label>
             <br></br>
-            <input type="checkbox" id="height" name="height" value="true"/>
+            <input type="checkbox" id="height" name="height" onClick={handleCheckbox} value={questions.height}/>
             <label for="height">Height</label>
             <br></br>
-            <input type="checkbox" id="water-intake" name="water-intake" value="true"/>
+            <input type="checkbox" id="water-intake" name="water-intake" onClick={handleCheckbox} value={questions.waterIntake}/>
             <label for="water-intake">Water intake</label>
             <br></br>
-            <input type="checkbox" id="steps" name="steps" value="true"/>
+            <input type="checkbox" id="steps" name="steps" onClick={handleCheckbox} value={questions.steps}/>
             <label for="steps">Steps</label>
             <br></br>
-            <input type="checkbox" id="screen-time" name="screen-time" value="true"/>
+            <input type="checkbox" id="screen-time" name="screen-time" onClick={handleCheckbox} value={questions.screenTime}/>
             <label for="screen-time">Screen time</label>
             <br></br>
-            <input type="checkbox" id="heart-rate" name="heart-rate" value="true"/>
+            <input type="checkbox" id="heart-rate" name="heart-rate" onClick={handleCheckbox} value={questions.heartRate}/>
             <label for="heart-rate">Heart rate</label>
             <br></br>
-            <input type="checkbox" id="blood-oxygen" name="blood-oxygen" value="true"/>
+            <input type="checkbox" id="blood-oxygen" name="blood-oxygen" onClick={handleCheckbox} value={questions.bloodOxygen}/>
             <label for="blood-oxygen">Blood oxygen level</label>
             <br></br>
-            <input type="checkbox" id="sleep-amount" name="sleep-amount" value="true"/>
+            <input type="checkbox" id="sleep-amount" name="sleep-amount" onClick={handleCheckbox} value={questions.sleepAmount}/>
             <label for="sleep-amount">Sleep amount</label>
             <br></br>
             <button type="submit">Submit</button>
